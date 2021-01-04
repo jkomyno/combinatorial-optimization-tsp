7904e09c8f7ab266fcf0781cbcfa38ba1589cc28
jkomyno <skiabo97@gmail.com>
2021-01-04 19:57:45 +0100

    ex2-metaheuristic: keep track of average cost of solutions

---
+++ ex2-metaheuristic/statistics.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <algorithm>
+#include <vector>
+
+#include "PermutationPath.h"
+
+namespace statistics {
+    template <typename T>
+    T sum_cost(const std::vector<PermutationPath<T>>& population_pool) noexcept {
+        return std::accumulate(
+            population_pool.cbegin(), population_pool.cend(), T(0.0),
+            [](T cost, const PermutationPath<T>& path) -> T { return cost + path.cost(); });
+    }
+
+    template <typename T>
+    T average_cost(const std::vector<PermutationPath<T>>& population_pool) noexcept {
+        return sum_cost(population_pool) / static_cast<T>(population_pool.size());
+    }
+
+}  // namespace statistics
